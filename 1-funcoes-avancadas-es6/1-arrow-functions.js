ef7e36b2d04f7572ee900023ece730e9940ebf3b
Murillo ROmeu <murillosi@gmail.com>
2021-07-18 14:31:07 -0300

    arrow functions

---
+++ 1-funcoes-avancadas-es6/1-arrow-functions.js
@@ -0,0 +1,20 @@
+// retorno simples
+var sum = (a, b)  => a + b;
+
+console.log(sum(1, 3));
+
+// outras operaçoes antes do retorno
+var sub = (a, b) => {
+  if (a<b) {
+    return b - a;
+  } else {
+    return a - b;
+  }
+}
+
+console.log(sub(3, 2));
+
+// apenas um argumento pode ser passado direto
+var quadrado = a => a * a;
+
+console.log(quadrado(5));
